70b442e502852c45fab5d646424ff3634cfc908c
efc <efc@clst.org>
2026-05-19 12:01:58 -0500

    custom field conditions

--- resources/js/addon.js
+++ resources/js/addon.js
@@ -1,5 +1,11 @@
 import Note from './components/fieldtypes/Note.vue'
 
 Statamic.booting(() => {
-    Statamic.component('note-fieldtype', Note)
+    Statamic.component('note-fieldtype', Note);
+    Statamic.$conditions.add('showCascadingNote', ({ values }) => {
+        return values.cep_ancestor_tickets;
+    });
+    Statamic.$conditions.add('showAccessTickets', ({ values }) => {
+        return ! values.cep_ancestor_tickets?.startsWith("Protected by");
+    });
 })
